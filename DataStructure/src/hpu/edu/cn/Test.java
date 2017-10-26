4f738360804d4bd30515e959984d8a3e1dc6e5f2
dongleqiang <dlq_tinysoft@163.com>
2017-10-26 13:32:38 +0800

    创建了Test类

---
+++ DataStructure/src/hpu/edu/cn/Test.java
@@ -0,0 +1,10 @@
+package hpu.edu.cn;
+
+/**
+ * Created by 董乐强 on 2017/10/26.
+ */
+public class Test {
+    public static void main(String [] args){
+        System.out.println("hello world");
+    }
+}
